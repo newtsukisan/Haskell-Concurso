e0673a7be9ae7c7ba5671f27e1f4c3e94d4363f8
Jose A <newtsukisan@gmail.com>
2015-11-25 14:00:19 +0100

    Using quickcheck.
    Limit length of the testing list for a time size useful.

--- src/Countdown.lhs
+++ src/Countdown.lhs
@@ -2,7 +2,8 @@ Countdown example from chapter 11 of Programming in Haskell,
 Graham Hutton, Cambridge University Press, 2007.
 
 > module Countdown
->    where
+>  (subs, perms, choices
+>  ) where
 > import System.CPUTime
 > import Numeric
 > import System.IO
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
 import Test.QuickCheck
+import Countdown
 
 main :: IO ()
 main = defaultMain tests
@@ -10,16 +11,25 @@ main = defaultMain tests
 tests = [
         testGroup "Probando la funcionalidad basica de choices" 
               [
-                testProperty "ver si funciona" prop1
+                testProperty "Definicion basica" prop1
               , testProperty "ver si funciona" prop2
               ]
         ]
 
-prop1 b = foldr (&&) True b == all (==True) b
-  where types = (b :: [Bool])
+prop1 (SmallIntList xs) = choices xs == (concat $ map perms $ subs xs) 
+  
 
 prop2 i = i == i + 0
   where types = (i :: Int) 
 
 -- choices to be tested
--- one posible implementation is choices = concat . perms $ map subs 
+-- one posible implementation is choices = concat . perms $ map subs
+
+newtype SmallIntList = SmallIntList [Int] deriving (Eq,Show)
+
+instance Arbitrary SmallIntList where
+  arbitrary = sized $ \s -> do
+                 n <- choose (0,s `min` 9)
+                 xs <- vectorOf n (choose (-10000,10000))
+                 return (SmallIntList xs)
+  shrink (SmallIntList xs) = map SmallIntList (shrink xs) 
