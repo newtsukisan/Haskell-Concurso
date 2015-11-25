d6be8a41919fb4a6a89a22fa3958742dc78ad9e1
Jose A <newtsukisan@gmail.com>
2015-11-25 13:41:12 +0100

    Add module from course
    Preparing for testing module from course.

--- app/Main.hs
+++ app/Main.hs
@@ -9,15 +9,11 @@ import System.Environment  -- Para manejarnos con la entrada-salida
 -- Para calcular los tiempos que podemos tardamos en encontrar las soluciones
 -- time stack exec concurso-exe [1,3,7,10,25,50] 765 1
 
-
-
- 
-
 ejecutar       :: [String] -> String
 ejecutar (x0:x1:x2:[])
    | x2 == "1" = sub_ejecuta solucion  x0 x1    -- Solucion propia
    | x2 == "2" = sub_ejecuta solutions x0 x1    -- Solucion del libro no mejorada
-   | otherwise = "No se ha seleccionado uno de los dos  metodos"
+   | otherwise = "No se ha seleccionado uno de los dos  métodos disponibles"
       where 
          sub_ejecuta f arg0 arg1 = show $ f (getEstado (arg0))   (getObjetivo (arg1))
          getEstado     :: String -> [Int]       -- Para obtener el estado inicial
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,14 +8,18 @@ main :: IO ()
 main = defaultMain tests
 
 tests = [
-        testGroup "Sorting Group 1" [
-                testProperty "prop1" prop1,
-                testProperty "prop2" prop2
-           ]
-      ]
+        testGroup "Probando la funcionalidad basica de choices" 
+              [
+                testProperty "ver si funciona" prop1
+              , testProperty "ver si funciona" prop2
+              ]
+        ]
 
 prop1 b = foldr (&&) True b == all (==True) b
   where types = (b :: [Bool])
 
-prop2 i = i == i + 3
-  where types = (i :: Int)
+prop2 i = i == i + 0
+  where types = (i :: Int) 
+
+-- choices to be tested
+-- one posible implementation is choices = concat . perms $ map subs 
